5073250e6f3b596b9ea018e80b52b28046f970e2
kouta0530 <16jk047@ymklab.net>
2021-05-14 10:24:13 +0900

    feat(webpack): Asset Management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import _ from 'lodash';
+import "./style.css"
+import Icon from "./icon.png"
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
+
+
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello')
+
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
